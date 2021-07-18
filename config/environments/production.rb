c20b702eb0999b5f1d172d5bab7c6c18492c1fd3
RanX12 <1726449139@qq.com>
2021-07-18 19:42:38 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -14,57 +16,94 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  # config.require_master_key = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # config.action_controller.asset_host = 'http://assets.example.com'
   if ENV['CDN'].present?
     config.action_controller.asset_host = ENV['CDN']
   end
-
-  config.active_storage.service = :upyun
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Mount Action Cable outside main process or domain
-  config.action_cable.allowed_request_origins = [ "#{ENV['PROTOCOL']}://#{ENV['DOMAIN']}" ]
+  # Mount Action Cable outside main process or domain.
+  config.action_cable.allowed_request_origins = ["#{ENV['PROTOCOL']}://#{ENV['DOMAIN']}"]
+  config.action_cable.worker_pool_size = 8
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  # Action Cable log tags
+  config.action_cable.log_tags = [->(request) { request.uuid }]
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
 
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "qinglion_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  # Lograge
+  config.lograge.enabled = true
+  config.lograge.base_controller_class = ['ActionController::API', 'ActionController::Base']
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.lograge.keep_original_rails_log = true
+  config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
+
+  config.lograge.custom_payload do |controller|
+    if controller.respond_to?(:current_user, true)
+      uid = controller.send(:current_user)&.id
+    elsif controller.respond_to?(:current_admin, true)
+      uid = controller.send(:current_admin)&.id
+    end
+
+    if controller.respond_to?(:current_team, true)
+      tid = controller.send(:current_team)&.id rescue nil
+    end
+
+    request = controller.request
+
+    {
+      request_id: request.uuid,
+      host: request.host,
+      ip: request.env['HTTP_X_FORWARDED_FOR'] || request.env['REMOTE_ADDR'],
+      ua: request.env['HTTP_USER_AGENT'],
+      uid: uid,
+      tid: tid,
+      sid: request.session&.id&.to_s
+    }
+  end
+  config.lograge.custom_options = lambda do |event|
+    payload =
+      if event.name == 'perform_action.action_cable'
+        { params: event.payload[:data].to_json }.merge(event.payload.slice(:request_id, :host, :ip, :ua, :uid, :sid))
+      else
+        { params: event.payload[:params].to_json }
+      end
+
+    payload
+  end
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -73,59 +112,9 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
-  # logger           = ActiveSupport::Logger.new(STDOUT)
-  # logger.formatter = config.log_formatter
-  # config.logger = ActiveSupport::TaggedLogging.new(logger)
-
-  config.logger = Logger.new (Rails.root.join ("log/production.log"))
-
-  if ENV['EXCEPTION_SLACK_WEBHOOK'].present?
-    config.middleware.use ExceptionNotification::Rack,
-      :slack => {
-        :webhook_url => ENV['EXCEPTION_SLACK_WEBHOOK'],
-        :channel => "#exceptions",
-        :username => ENV['EXCEPTION_USERNAME'].presence || 'webhook',
-        :additional_parameters => {
-          :mrkdwn => true,
-        }
-      }
-  end
-  config.lograge.enabled = true
-  config.lograge.base_controller_class = ['ActionController::API', 'ActionController::Base']
-  config.lograge.formatter = Lograge::Formatters::Logstash.new
-  config.lograge.custom_payload do |controller|
-    devise_info = nil
-    if controller.respond_to?(:devise_info, true)
-      devise_info = controller.send(:devise_info)
-    end
-    request = controller.request
-    {
-      host: request.host,
-      ip: request.env['HTTP_X_FORWARDED_FOR'] || request.env['REMOTE_ADDR'],
-      ua: request.env['HTTP_USER_AGENT'],
-      # user id
-      devise_info: devise_info,
-      # session id
-      sid: request.session&.id
-    }
-  end
-  config.lograge.custom_options = lambda do |event|
-    {
-      params: event.payload[:params]&.except(:controller, :action, :format)
-    }
-  end
-
-  config.lograge.enabled = true
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
